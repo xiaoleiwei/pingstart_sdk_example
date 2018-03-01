e9c817c2bf60e1cacb25b464a91415c13effa780
liuchong <liuchong@newborntown.com>
2018-03-01 19:26:46 +0800

    add v3.7.2sample

---
+++ pingstart_v3.7.2sample/src/main/java/com/pingstart/mediation/MainActivity.java
@@ -0,0 +1,65 @@
+package com.pingstart.mediation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.pingstart.adsdk.PingStartSDK;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PingStartSDK.initializeSdk(this, "5079", "5079_55");
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.show_banner_ad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BannerActivity.class));
+            }
+        });
+
+        findViewById(R.id.show_inter_ad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, InterActivity.class));
+            }
+        });
+
+        findViewById(R.id.show_native_ad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NativeActivity.class));
+            }
+        });
+
+        findViewById(R.id.show_multi_ad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MultiActivity.class));
+            }
+        });
+
+        findViewById(R.id.show_pingstart_adswall).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AdsWallActivity.class));
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
